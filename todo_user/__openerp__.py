b01195feeb2c26344124c60b1bf4f5e9c74e3c60
Alexander Voitenko <voitenko@gmail.com>
2015-05-20 19:33:58 +0300

    Moved todo_app to this repos. Added new module

---
+++ todo_user/__openerp__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'Multiuser To-Do',
+    'description': 'Extend the To-do app to multiuser.',
+    'author': 'Daniel Reis',
+    'depends': ['todo_app'],
+}
---
+++ todo_user/todo_task.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+class TodoTask(models.Model):
+    _inherit = 'todo.task'
+
+    user_id = fields.Many2one('res.users', 'Responsible')
+    date_deadline = fields.Date('Deadline')
+    name = fields.Char(help="What needs to be done?")
